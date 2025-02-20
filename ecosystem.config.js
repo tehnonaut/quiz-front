e779429a8630dd02a1c9e28d10400d7c3a58c142
Igor Miladinovic <miladinovic87@gmail.com>
2025-02-21 00:28:45 +0100

    new style

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,26 @@
-module.exports = {
-    apps: [{
-      name: 'quiz.tools',   
-      script: 'npm',
-      args: 'start',
-      cwd: '/',
-      instances: 'max',
-      exec_mode: 'cluster',
-      autorestart: true,
-      watch: '.',
-      env: {
-        NODE_ENV: 'production',
-      },
-    }],
-  };
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const siteName = process.env.PROJECT_NAME || 'quiz.tools';
+
+const ecosystem = {
+	apps: [
+		{
+			name: siteName,
+			script: 'npm',
+			args: 'start',
+			cwd: '/',
+			instances: 'max',
+			exec_mode: 'cluster',
+			autorestart: true,
+			watch: '.',
+			log_date_format: 'YYYY-MM-DD HH:mm Z',
+			env: {
+				NODE_ENV: 'production',
+			},
+		},
+	],
+};
+
+export default ecosystem;
