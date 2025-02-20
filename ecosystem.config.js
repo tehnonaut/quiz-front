57c025eef09a9d1e97466f0db1cf89f9d0e02904
Igor Miladinovic <miladinovic87@gmail.com>
2025-02-21 00:00:30 +0100

    newest export

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,18 @@
 module.exports = {
-  apps: [
-    {
-      name: 'quiz.tools',
-      script: './dist/app.js',
-      instances: 'max',
-      exec_mode: 'cluster',
-      watch: false,
-      max_memory_restart: '1G',
-      env: {
-        NODE_ENV: 'production',
-        PORT: 8090,
-      }
-    }
-  ]
-}; 
+    apps: [
+      {
+        name: "quiz.tools",
+        script: "npm",
+        args: "start",
+        cwd: "/",
+        instances: "max",
+        exec_mode: "cluster",
+        autorestart: true,
+        watch: false,
+        max_memory_restart: "1G",
+        env: {
+          NODE_ENV: "production",
+        },
+      },
+    ],
+  };
