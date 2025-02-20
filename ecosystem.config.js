41675651029f4d393a7f54dbc5d71530cb558451
Igor Miladinovic <miladinovic87@gmail.com>
2025-02-20 23:35:57 +0100

    eco to old style

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    apps: [
+      {
+        name: "quiz.tools",
+        script: "node dist/app.js",
+        args: "start",
+        instances: "max",
+        exec_mode: "cluster",
+        watch: false,
+        env: {
+          NODE_ENV: "production",
+          PORT: 8090,
+        },
+      },
+    ],
+  };
