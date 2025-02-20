6ee2d5379cabb0cc6a37e2fe61a451683816bf39
Igor Miladinovic <miladinovic87@gmail.com>
2025-02-21 00:05:01 +0100

    ecosystem structure

--- ecosystem.config.mjs
+++ ecosystem.config.mjs
@@ -1,20 +1,20 @@
 const ecosystem = {
-    apps: [
-        {
-        name: "quiz.tools",
-        script: "npm",
-        args: "start",
-        cwd: "/",
-        instances: "max",
-        exec_mode: "cluster",
-        autorestart: true,
-        watch: false,
-        max_memory_restart: "1G",
-        env: {
-            NODE_ENV: "production",
-        },
-        },
-    ],
+  apps: [
+    {
+      name: "quiz.tools",
+      script: "npm",
+      args: "start",
+      cwd: "/",
+      instances: "max",
+      exec_mode: "cluster",
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
 };
 
-  export default ecosystem;
+export default ecosystem;
