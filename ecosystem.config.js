57b84d6c814c501488ddf5fe68ec689d7d3c0b3f
Igor Miladinovic <miladinovic87@gmail.com>
2025-02-20 23:31:05 +0100

    ecosystem auto deployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,16 @@
-import dotenv from 'dotenv';
+import * as dotenv from 'dotenv';
 dotenv.config();
 
 const siteName = process.env.PROJECT_NAME || 'Web Site';
 
-const Config = 
-    {
-        apps: [
-            {
-                name: siteName,
-                script: 'dist/app.js',
-                log_date_format: 'YYYY-MM-DD HH:mm Z',
-            },
-        ],
-    }    
+const Config = {
+    apps: [
+        {
+            name: siteName,
+            script: 'dist/app.js',
+            log_date_format: 'YYYY-MM-DD HH:mm Z',
+        },
+    ],
+};
 
 export default Config;
